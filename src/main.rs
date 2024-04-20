9ca180f90257e41e45f2d24da4861a4c5885a0be
Sid <siddpal04@gmail.com>
2024-04-21 03:17:18 +0530

    feat: entry point to the cli

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,37 @@
+use crate::cli::display;
+use crate::cli::get_weather_info;
+use colored::*;
+use dotenv::dotenv;
+use std::env;
+use std::io;
+
 mod cli;
 fn main() {
-    println!("Hello, world!");
+    dotenv().ok();
+
+    let api_key = env::var("OPENWEATHER_API_KEY").unwrap();
+
+    println!("{}", "Welcome to Weather Warp".bright_cyan());
+    println!("Enter your city: ");
+    let mut city = String::new();
+    io::stdin()
+        .read_line(&mut city)
+        .expect("Failed to read input...");
+    let city = city.trim();
+
+    println!("Enter your country code (eg. IND for India): ");
+    let mut country = String::new();
+    io::stdin()
+        .read_line(&mut country)
+        .expect("Failed to read input...");
+    let country = country.trim();
+
+    match get_weather_info(&city, &country, &api_key) {
+        Ok(response) => {
+            display(&response);
+        }
+        Err(e) => {
+            eprintln!("{}", e);
+        }
+    };
 }
