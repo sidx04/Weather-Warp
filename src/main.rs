00cf7affd523683ba198945cab696b34c2a7b065
Sid <siddpal04@gmail.com>
2024-05-01 01:45:51 +0530

    feat: accept country and code from command line args

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,54 @@
-use crate::cli::display;
-use crate::cli::get_weather_info;
 use colored::*;
+use configure::configure;
 use dotenv::dotenv;
 use std::env;
-use std::io;
 
 mod cli;
+use crate::cli::{display, get_weather_info};
+
+mod args;
+
+mod configure;
 fn main() {
     dotenv().ok();
 
-    let api_key = env::var("OPENWEATHER_API_KEY").unwrap();
-    let api_url = env::var("OPENWEATHER_API_URL").unwrap();
+    let args = args::Args::arg_parse();
 
-    println!("{}", "Welcome to Weather Warp.".bright_cyan());
-    println!("Enter your city: ");
-    let mut city = String::new();
-    io::stdin()
-        .read_line(&mut city)
-        .expect("Failed to read input...");
-    let city = city.trim();
-
-    println!("Enter your country code (eg. IND for India): ");
-    let mut country = String::new();
-    io::stdin()
-        .read_line(&mut country)
-        .expect("Failed to read input...");
-    let country = country.trim();
-
-    match get_weather_info(&city, &country, &api_url, &api_key) {
-        Ok(response) => {
-            display(&response);
-        }
-        Err(e) => {
-            eprintln!("{}", e);
+    // If no command is provided, execute the `Weather` command by default
+    let command = match args.command {
+        Some(c) => c,
+        None => {
+            let default_city = Some(String::from("Kolkata"));
+            let default_country = Some(String::from("IND"));
+            args::Command::Weather {
+                city: default_city,
+                country: default_country,
+            }
         }
     };
+
+    match command {
+        args::Command::Weather { city, country } => {
+            let api_key = env::var("OPENWEATHER_API_KEY").unwrap();
+            let api_url = env::var("OPENWEATHER_API_URL").unwrap();
+
+            println!("{}", "Welcome to Weather Warp.".bright_cyan());
+
+            // Extract the city and country from the Option
+            let city = city.unwrap_or_else(|| String::from("Kolkata"));
+            let country = country.unwrap_or_else(|| String::from("IND"));
+
+            match get_weather_info(&city, &country, &api_url, &api_key) {
+                Ok(response) => {
+                    display(&response);
+                }
+                Err(e) => {
+                    eprintln!("{}", e);
+                }
+            };
+        }
+        args::Command::Config { city, country } => {
+            configure(city.unwrap(), country.unwrap());
+        }
+    }
 }
